299a28048ff947bf4fc68d8be1d8118770c03912
yumekiti <yumekiti120440@gmail.com>
2023-08-18 12:18:03 +0900

    fix: navigation click and refactoring

---
+++ src/constants/text.ts
@@ -0,0 +1,5 @@
+const footerText = '&copy; 2023 - Copyright Yumekiti.';
+
+const welcomeText = 'ようこそ、ゆめきちのブログへ！\n\n自由気ままにブログを上げていきます興味がある記事があれば見ていってください！\n\nTwitter: @yumekiti1204\nGithub: @yumekiti'
+
+export { footerText, welcomeText };
